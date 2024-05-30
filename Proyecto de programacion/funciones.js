31dfcb305d35ddbbd1bce4d2ccfdc89f8433a590
jacgCG <josuecastrogonzalesxd@gmail.com>
2024-05-30 07:50:25 -0500

    proyecto-ti

--- Proyecto de programacion/funciones.js
+++ Proyecto de programacion/funciones.js
@@ -1,9 +1,25 @@
-const selectCategorias = document.getElementById('select-categorias');
+// Archivo: funciones.js
 
-selectCategorias.addEventListener('mouseover', () => {
-  selectCategorias.style.display = 'block';
-});
+let slideIndex = {
+  'productos': 0,
+  'categorias': 0,
+  'historial-compras': 0,
+  'historial-ventas': 0,
+  'ofertas': 0
+};
 
-selectCategorias.addEventListener('mouseout', () => {
-  selectCategorias.style.display = 'none';
-});
+function moveSlider(sectionId, direction) {
+  const slider = document.getElementById(`${sectionId}-slider`);
+  const slides = slider.getElementsByClassName('slide');
+  const totalSlides = slides.length;
+
+  slideIndex[sectionId] += direction;
+
+  if (slideIndex[sectionId] >= totalSlides) {
+      slideIndex[sectionId] = 0;
+  } else if (slideIndex[sectionId] < 0) {
+      slideIndex[sectionId] = totalSlides - 1;
+  }
+
+  slider.style.transform = `translateX(-${slideIndex[sectionId] * 100}%)`;
+}
